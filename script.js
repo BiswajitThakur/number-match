d2f9fd189ec195178991f19d92b27b3de4f53b50
BiswajitThakur <biswajit733127@gmail.com>
2024-10-06 21:44:12 +0530

    _

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
 
-let game_table = null;
+let game_table = null; // root element of game table
+
 let game_elements = null;
+
+// For Undo Redo
 let input_history = [];
 let undo = [];
 
@@ -104,28 +107,92 @@ function select_rand_from_metrix(size) {
   return [Math.floor(Math.random() * size), Math.floor(Math.random() * size)];
 }
 
-function create_node_elm(val) {
-  const t_or_f = Math.floor(Math.random() * 3) == 0 ? true : false;
-  const attr_name = "data-is";
+function encode_game(elm) {
+  let raws = [];
+  elm.forEach((e) => {
+    let raw = [];
+    e.forEach((v) => {
+      if (!v.disabled) {
+        raw.push(`i${v.getAttribute("data-is")}`);
+      } else {
+        raw.push(`${v.getAttribute("data-is")}`);
+      }
+    });
+    raws.push(raw.join("|"));
+  });
+  return btoa(raws.join("-"));
+}
 
-  if (!t_or_f) {
-    const elem = document.createElement("span");
-    elem.setAttribute(attr_name, val);
-    elem.textContent = val;
-    elem.style.width = "100%";
-    return elem;
+function gen_lvl(size, lvl) {
+  const total = size * size;
+  let to_rm = 0;
+  if (lvl < 2) {
+    to_rm = Math.floor(total * (1 / 3));
+  } else if (lvl == 2) {
+    to_rm = Math.floor(total * (1 / 2));
   } else {
+    to_rm = Math.floor(total * (2 / 3));
+  }
+  let arr = [];
+  for (let i = 0; i < size; i++) {
+    let a = [];
+    for (let j = 0; j < size; j++) {
+      a.push(false);
+    }
+    arr.push(a);
+  }
+  let true_count = 0;
+  let total_try = 0;
+  while (total_try < 900000) {
+    const ranw_raw = Math.floor(Math.random() * size);
+    const rand_coll = Math.floor(Math.random() * size);
+    if (!arr[rand_coll][ranw_raw]) {
+      arr[rand_coll][ranw_raw] = true;
+      true_count += 1;
+    }
+    total_try += 1;
+    if (true_count >= to_rm) {
+      break;
+    }
+  }
+  const attr_name = "data-is";
+  return function (r, c, val) {
     const input = document.createElement("input");
     input.setAttribute(attr_name, val);
     input.setAttribute("type", "number");
     input.style.width = "100%";
     input.style.overflow = "hidden";
     input.style.textAlign = "center";
+    if (arr[r][c]) {
+      input.addEventListener("change", () => {
+        push_input_history(input);
+      });
+    } else {
+      input.value = val;
+      input.disabled = true;
+    }
+    return input;
+  };
+}
+
+function create_node_elm(val, lvl) {
+  const t_or_f = Math.floor(Math.random() * lvl) == 0 ? true : false;
+  const attr_name = "data-is";
+  const input = document.createElement("input");
+  input.setAttribute(attr_name, val);
+  input.setAttribute("type", "number");
+  input.style.width = "100%";
+  input.style.overflow = "hidden";
+  input.style.textAlign = "center";
+  if (t_or_f) {
+    input.value = val;
+    input.disabled = true;
+  } else {
     input.addEventListener("change", () => {
       push_input_history(input);
     });
-    return input;
   }
+  return input;
 }
 
 function import_game(val) {
@@ -138,103 +205,80 @@ function import_game(val) {
   if (raws.length < 2) {
     return;
   }
-  let root = document.createElement("table");
-  root.id = "my-table";
-  game_elements = [];
+  let root = document.createElement("div");
+  root.id = "game";
+  let elements = [];
   raws.forEach((raw) => {
-    let table_raw = document.createElement("tr");
+    let table_raw = document.createElement("div");
+    table_raw.className = "game-raws";
     let rr = [];
     raw.forEach((v) => {
-      let td = document.createElement("td");
+      let td = document.createElement("input");
+      const val = v.replace(/^i/, "");
+      td.setAttribute("data-is", val);
+      td.setAttribute("type", "number");
+      td.style.textAlign = "center";
       if (/^i\d+$/.test(v)) {
-        let input = document.createElement("input");
-        input.setAttribute("data-is", v.replace(/^i/, ""));
-        input.setAttribute("type", "number");
-        input.style.width = "100%";
-        input.style.overflow = "hidden";
-        input.style.textAlign = "center";
-        input.addEventListener("change", () => {
-          push_input_history(input);
+        td.addEventListener("change", () => {
+          push_input_history(td);
         });
-        rr.push(input);
-        td.appendChild(input);
-        table_raw.appendChild(td);
-        return;
       } else if (/^\d+$/.test(v)) {
-        const elem = document.createElement("span");
-        elem.setAttribute("data-is", v);
-        elem.textContent = v;
-        elem.style.width = "100%";
-        rr.push(elem);
-        td.appendChild(elem);
-        table_raw.appendChild(td);
-        return;
+        td.value = val;
+        td.disabled = true;
+      } else {
+        throw new Error("ERROR: invalid hash.");
       }
-      throw new Error("ERROR: invalid hash.");
+      rr.push(td);
+      table_raw.appendChild(td);
     });
-    game_elements.push(rr);
+    elements.push(rr);
     root.appendChild(table_raw);
   });
-  const g = document.getElementById("my_game");
   if (game_table) {
     game_table.remove();
   }
   game_table = root;
+  const g = document.getElementById("game_cnt");
   g.appendChild(root);
-}
-try {
-  import_game(window.location.hash);
-} catch (err) {
-  console.error(err);
+  return elements;
 }
 
-function encode_game(elm) {
-  let raws = [];
-  elm.forEach((e) => {
-    let raw = [];
-    e.forEach((v) => {
-      if (v.tagName === "INPUT") {
-        raw.push(`i${v.getAttribute("data-is")}`);
-      } else {
-        raw.push(`${v.getAttribute("data-is")}`);
-      }
-    });
-    raws.push(raw.join("|"));
-  });
-  return btoa(raws.join("-"));
-}
-
-function createRoot(size) {
+function createRoot(size, lvl) {
   if (size > 13) {
     alert("Size must be less then or equal to 13");
     return;
   }
-  const g = document.getElementById("my_game");
-  if (game_table) {
-    game_table.remove();
-  }
-  let root = document.createElement("table");
-  root.id = "my-table";
+  let root = document.createElement("div");
+  root.id = "game";
   let my_table = create_mx(size);
-  game_elements = [];
-  my_table.forEach((m) => {
-    let tr = document.createElement("tr");
+  let my_lvl = gen_lvl(size, lvl);
+  let elements = [];
+  for (let i = 0; i < size; i++) {
+    let tr = document.createElement("div");
+    tr.className = "game-raws";
     let tr_arr = [];
-    m.forEach((n) => {
-      let td = document.createElement("td");
-      const e = create_node_elm(n);
-      td.appendChild(e);
+    for (let j = 0; j < size; j++) {
+      const e = my_lvl(i, j, my_table[i][j]);
       tr_arr.push(e);
-      tr.appendChild(td);
-    });
+      tr.appendChild(e);
+    }
     root.appendChild(tr);
-    game_elements.push(tr_arr);
-  });
+    elements.push(tr_arr);
+  }
   game_table = root;
+  const g = document.getElementById("game_cnt");
   g.appendChild(root);
+  return elements;
 }
 
-const my_input = document.querySelector("#root > .my-input");
+try {
+  game_elements = import_game(window.location.hash);
+  document.getElementById("create_new_inp").value = game_elements.length;
+} catch (err) {
+  console.error(err);
+}
+
+const my_input = document.getElementById("create_new_inp");
 
 my_input.addEventListener("keyup", (e) => {
   if (!/^Digit\d+$/.test(e.code)) {
@@ -242,38 +286,46 @@ my_input.addEventListener("keyup", (e) => {
   }
 });
 
-document.querySelector("#root > .create-game").addEventListener("click", () => {
+document.getElementById("create_new").addEventListener("click", () => {
   const value = my_input.value.trim();
   if (/^\s*$/.test(value)) {
     return;
   }
+  if (game_table) {
+    game_table.remove();
+  }
   input_history = [];
   undo = [];
   const user_input = parseInt(value);
-  createRoot(user_input);
-  window.location.hash = encode_game(game_elements);
+  const lvl = document.getElementById("game_lvl").value;
+  //alert(lvl);
+  const elm = createRoot(user_input, parseInt(lvl));
+  game_elements = elm;
+  window.location.hash = encode_game(elm);
 });
 
-document.querySelector("#root > .reset").addEventListener("click", () => {
+document.getElementById("reset").addEventListener("click", () => {
   if (!game_elements || !Array.isArray(game_elements)) {
     return;
   }
+  input_history = [];
+  undo = [];
   game_elements.forEach((m) => {
     m.forEach((n) => {
-      if (n.tagName === "INPUT") {
+      if (!n.disabled) {
         n.value = "";
       }
     });
   });
 });
 
-document.querySelector("#root > .solve").addEventListener("click", () => {
+document.getElementById("solve").addEventListener("click", () => {
   if (!game_elements || !Array.isArray(game_elements)) {
     return;
   }
   game_elements.forEach((m) => {
     m.forEach((n) => {
-      if (n.tagName === "INPUT") {
+      if (!n.disabled) {
         n.value = n.getAttribute("data-is");
       }
     });
@@ -325,13 +377,14 @@ document.getElementById("varify").addEventListener("click", () => {
   let is_win = true;
   game_elements.forEach((m) => {
     m.forEach((n) => {
-      if (n.tagName === "INPUT") {
+      if (!n.disabled) {
         if (n.value !== n.getAttribute("data-is")) {
           is_win = false;
         }
       }
     });
   });
+  alert("This features is under development.\nIt's not 100% accurate.");
   if (is_win) {
     alert("You win");
   } else {
